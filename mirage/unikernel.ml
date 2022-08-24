feabe6120f30d698d24653a6035703c42f7283fd
rand00 <oth.rand@gmail.com>
2022-08-24 10:29:31 +0200

    mirage/Unikernel: Moved over logic to lib

--- mirage/unikernel.ml
+++ mirage/unikernel.ml
@@ -13,54 +13,13 @@ let lwt_result_flatten_result = function
 
 module Main (S : Tcpip.Stack.V4V6) = struct
 
-  module Listen = struct
-    
-    let tcp stack port =
-      let callback flow =
-        let dst, dst_port = S.TCP.dst flow in
-        Logs.info (fun m ->
-          m "new tcp connection from IP '%s' on port '%d'"
-            (Ipaddr.to_string dst) dst_port);
-        S.TCP.read flow >>= function
-        | Ok `Eof ->
-          Logs.info (fun f -> f "Closing connection!");
-          Lwt.return_unit
-        | Error e ->
-          Logs.warn (fun f ->
-            f "Error reading data from established connection: %a"
-              S.TCP.pp_error e);
-          Lwt.return_unit
-        | Ok (`Data b) ->
-          Logs.info (fun f ->
-            f "read: %d bytes:\n%s" (Cstruct.length b) (Cstruct.to_string b));
-          S.TCP.close flow
-      in
-      Mirage_runtime.at_exit (fun () ->
-        S.TCP.unlisten (S.tcp stack) ~port |> Lwt.return
-      );
-      S.TCP.listen (S.tcp stack) ~port callback
-
-    let udp stack port =
-      let callback ~src:_ ~dst ~src_port:_ data =
-        Logs.info (fun m ->
-          m "new udp connection from IP '%s' on port '%d'"
-            (Ipaddr.to_string dst) port);
-        Logs.info (fun f ->
-          f "read: %d bytes:\n%s" (Cstruct.length data) (Cstruct.to_string data));
-        Lwt.return_unit
-      in
-      Mirage_runtime.at_exit (fun () ->
-        S.UDP.unlisten (S.udp stack) ~port |> Lwt.return
-      );
-      S.UDP.listen (S.udp stack) ~port callback
-
-  end
-
+  module Ct = Conntest.Make(S)
+  
   let try_register_listener ~stack input =
     begin match input with
       | "tcp" :: port :: [] ->
         begin match int_of_string_opt port with
-          | Some port -> Listen.tcp stack port |> Result.ok
+          | Some port -> Ct.Listen.tcp stack port |> Result.ok
           | None ->
             let msg =
               Fmt.str "Error: try_register_listener: Port '%s' is malformed" port
@@ -69,7 +28,7 @@ module Main (S : Tcpip.Stack.V4V6) = struct
         end
       | "udp" :: port :: [] ->
         begin match int_of_string_opt port with
-          | Some port -> Listen.udp stack port |> Result.ok
+          | Some port -> Ct.Listen.udp stack port |> Result.ok
           | None ->
             let msg =
               Fmt.str "Error: try_register_listener: Port '%s' is malformed" port
@@ -96,38 +55,6 @@ module Main (S : Tcpip.Stack.V4V6) = struct
       Logs.err (fun m -> m "Error: try_register_listener: %s" msg);
       exit 1
 
-  module Connect = struct
-
-    let tcp ~stack ~name ~port ~ip ~monitor_bandwidth =
-      begin
-        let open Lwt_result.Infix in
-        S.TCP.create_connection (S.tcp stack) (ip, port)
-        |> Lwt_result.map_error (fun err -> `Connection err)
-        >>= fun flow ->
-        Mirage_runtime.at_exit (fun () -> S.TCP.close flow);
-        let data = Cstruct.of_string @@ "I'm "^name in
-        S.TCP.write flow data
-        |> Lwt_result.map_error (fun err -> `Write err)
-      end
-      |> Lwt_result.map_error (fun err -> `Tcp err)
-
-    (*goto
-      * this should loop sending packets
-        * and retry + print on some failure
-          * ! have in mind that with notty one can't print
-            * - so this should later be replaced by updating react graph with error
-      * this should send the index of the packet
-        * this way one can count what packets has been lost @ listener
-      * the listener should send a packet back with same index (right away)
-        * so this conntest can check what (roundtrip) latency was of that packet-index
-    *)
-    let udp ~stack ~name ~port ~ip ~monitor_bandwidth =
-      let data = Cstruct.of_string @@ "I'm "^name in
-      S.UDP.write ~dst:ip ~dst_port:port (S.udp stack) data
-      |> Lwt_result.map_error (fun err -> `Udp err)
-    
-  end
-  
   let try_initiate_connection ~stack ~name uri_str =
     begin
       let uri = Uri.of_string uri_str in
@@ -172,8 +99,8 @@ module Main (S : Tcpip.Stack.V4V6) = struct
         ) (Ok false)
       in
       match protocol with
-      | `Tcp -> Connect.tcp ~stack ~name ~port ~ip ~monitor_bandwidth
-      | `Udp -> Connect.udp ~stack ~name ~port ~ip ~monitor_bandwidth
+      | `Tcp -> Ct.Connect.tcp ~stack ~name ~port ~ip ~monitor_bandwidth
+      | `Udp -> Ct.Connect.udp ~stack ~name ~port ~ip ~monitor_bandwidth
     end
     |> lwt_result_flatten_result >>= function
     | Ok () -> Lwt.return_unit
@@ -181,7 +108,8 @@ module Main (S : Tcpip.Stack.V4V6) = struct
       Logs.err (fun m -> m "Error: try_initiate_connection: %s" msg);
       exit 1
     | Error (`Udp udp_err) ->
-      Logs.err (fun m -> m "Error: try_initiate_connection: %a"
+      Logs.err (fun m ->
+        m "Error: try_initiate_connection: %a"
           S.UDP.pp_error udp_err
       );
       exit 1
