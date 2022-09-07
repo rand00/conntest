7117e357376b06841bcdf707321e7d62d1fec07c
rand00 <oth.rand@gmail.com>
2022-09-07 12:05:27 +0200

    mirage/Config: pinning contest to dev again

--- mirage/config.ml
+++ mirage/config.ml
@@ -57,8 +57,8 @@ let keys = [
  *     ~sublibs:["mirage"] *)
 
 let packages = [
-  (* package "conntest" ~pin:"git+https://github.com/rand00/conntest.git"; *)
-  package "conntest" ~pin:"git+https://github.com/rand00/conntest.git#20220906_WIP";
+  package "conntest" ~pin:"git+https://github.com/rand00/conntest.git";
+  (* package "conntest" ~pin:"git+https://github.com/rand00/conntest.git#20220906_WIP"; *)
   (*< add commit to string when release? e.g. #3c85fff2aba1bbf0d0e7f05427d7e41f9b7a7cc3*)
   package "uri";
   package "notty" ~pin:"git+https://github.com/rand00/notty.git#414_w_mirage"
