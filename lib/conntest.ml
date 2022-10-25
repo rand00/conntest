38d6038316fdc1991ba84a4d90bd3b3bf9a6899d
rand00 <oth.rand@gmail.com>
2022-10-25 17:29:44 +0200

    WIP: Implenting Udp_flow - still need async packet-distribution and packet-order handling

--- lib/conntest.ml
+++ lib/conntest.ml
@@ -7,9 +7,6 @@ let (let*) = Result.bind
 let (let+) x f = Result.map f x 
 
 module type S = sig
-
-  (* type stack *)
-  (* type udp_error *)
   
   module Listen : sig
 
@@ -53,7 +50,12 @@ module Make
     (O : Output.S)
 = struct
 
-  (* type stack = S.t *)
+  let error_to_msg pp v =
+    v >>= function
+    | Ok v -> Lwt_result.return v
+    | Error err ->
+      let msg = Fmt.str "%a" pp err in
+      Lwt_result.fail @@ `Msg msg
 
   module Tcp_flow = struct
     include S.TCP
@@ -61,13 +63,6 @@ module Make
     type t = S.TCP.flow
 
     let tcp_stack = S.tcp Sv.stack
-    
-    let error_to_msg pp v =
-      v >>= function
-      | Ok v -> Lwt_result.return v
-      | Error err ->
-        let msg = Fmt.str "%a" pp err in
-        Lwt_result.fail @@ `Msg msg
 
     let listen ~port callback =
       S.TCP.listen tcp_stack ~port callback
@@ -88,6 +83,76 @@ module Make
   
   module Tcp = Protocol.Make(Time)(Tcp_flow)(O)(struct
     let subproto = `Tcp
+  end)
+
+  module Udp_flow = struct
+    (* include S.UDP *)
+
+    type t = {
+      source : Cstruct.t Mirage_flow.or_eof Lwt_mvar.t;
+      pier : Ipaddr.t;
+      pier_port : int;
+    }
+
+    let udp_stack = S.udp Sv.stack
+
+    (*> goto pass flow to callback*)
+    (*goto howto;
+      * read src-ip, src-port, packet-header-conn_id
+        * put these into some datastructure tracking all 'connections'
+          * brian; mutable?
+          * then some single recursive proc reads all input-datagrams
+            * and puts them into corresponding flow's based on conn-id
+    *)
+    let listen ~port user_callback =
+      let callback ~src ~dst ~src_port data =
+        match Packet.Tcp.init ~ignore_data:true data with
+        | Ok (`Done (packet, _rest)) ->
+          (*goto check/register connection-id -> flow*)
+          (*goto only call user-callback with flow on first packet recvd*)
+          let source = Lwt_mvar.create @@ `Data data in
+          let flow = { source; pier = src; pier_port = src_port } in
+          user_callback flow
+        (*goo*)
+        (*> goto change interface of 'listen' to return Result.t instead*)
+        | Ok (`Unfinished _) ->
+          failwith "Udp_flow: `Unfinished is unsupported for UDP"
+        | Error (`Msg err) ->
+          failwith ("Udp_flow: Error: "^err) 
+      in
+      S.UDP.listen udp_stack ~port callback
+
+    let unlisten ~port =
+      S.UDP.unlisten udp_stack ~port 
+
+    let create_connection (pier, pier_port) =
+      let source = Lwt_mvar.create_empty () in
+      Lwt_result.return { source; pier; pier_port }
+
+    let read flow =
+      Lwt_mvar.take flow.source >|= fun res ->
+      Ok res
+
+    (*> Note: it's important that all cstructs are written at once for ordering*)
+    let writev flow datas =
+      let data = Cstruct.concat datas in
+      let dst, dst_port = flow.pier, flow.pier_port in
+      (*< spec:
+        * listen-case: flow is only given to callback on recv first packet
+        * connect-case: flow already has dst + dst_port
+      *)
+      S.UDP.write ~dst ~dst_port udp_stack data
+      |> error_to_msg S.UDP.pp_error
+
+    let dst flow = flow.pier, flow.pier_port
+    
+    (*goto should more happen here?*)
+    let close flow = Lwt.return_unit
+    
+  end
+  
+  module Udp = Protocol.Make(Time)(Udp_flow)(O)(struct
+    let subproto = `Udp
   end)
 
   module Listen = struct
