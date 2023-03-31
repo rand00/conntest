367aac5d952efb19d2f84d7a2132826c317a8ca7
mercy <waringamercy552@gmail.com>
2023-03-31 12:05:23 +0300

    Revert "progress with errors"
    This reverts commit 3eadab32ba70ef9cb10471ca04e64985adacc4fd.

--- lib/conntest.ml
+++ lib/conntest.ml
@@ -347,7 +347,6 @@ module Make
                    .. and ring shouldn't be long
                 *)
                 Log.debug (fun m -> m "feed_source: packet_index_diff < 0");
-                (*changes*)
                 let rec loop i =
                   (*Define a recursive function loop that takes an index i as an argument.*)
                   match Ring.wrap_reverse_i len (i + 1) with
@@ -378,12 +377,8 @@ module Make
                      using wrap_reverse_i, and finally extracting the value from the resulting
                       Option using Option.get.*)
                 
-                   (*end of changes*)
+
                 let did_set = ref false in
-                
-            
-  
-                  
                 (*> goto perf; could use something like 'find_map_rev'
                   * which would try the latest packets first -
                     * which should be more often hit
